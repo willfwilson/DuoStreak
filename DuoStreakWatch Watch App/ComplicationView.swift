564cb3c7a58f8bfb2efe30a83d5dd1deeb53e768
Will Wilson <94478894+willfwilson@users.noreply.github.com>
2022-10-18 21:56:13 +0100

    Addition of Watchkit App

---
+++ DuoStreakWatch Watch App/ComplicationView.swift
@@ -0,0 +1,20 @@
+//
+//  ComplicationView.swift
+//  DuoStreakWatch Watch App
+//
+//  Created by William Wilson on 18/10/2022.
+//
+
+import SwiftUI
+
+struct ComplicationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ComplicationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComplicationView()
+    }
+}
---
+++ DuoStreakWatch Watch App/DuoStreakWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuoStreakWatchApp.swift
+//  DuoStreakWatch Watch App
+//
+//  Created by William Wilson on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DuoStreakWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
